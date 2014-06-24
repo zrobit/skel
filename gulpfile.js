32650dcbfd917ce3fbd237e5c4bdefd58418b48b
zrobit <luismigre47@gmail.com>
2014-06-23 23:22:18 -0500

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
     nib = require('nib'),
+    watch = require('gulp-watch'),
+    livereload = require('gulp-livereload'),
     handler = require('./handler.js');
 
 var paths = {
@@ -23,4 +25,10 @@ gulp.task('stylus', function () {
   gulp.src(paths.styles)
     .pipe(stylus({use: [nib()]}))
     .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('watch', function () {
+  gulp.src(['templates/**/*.jade', 'styles/**/*.styl'])
+    .pipe(watch())
+    .pipe(livereload());
 });
