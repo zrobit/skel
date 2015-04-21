a060d43958a3b12b4ad3556d964ac53f861374c3
zrobit <luismigre47@gmail.com>
2015-04-21 01:38:05 -0500

    add task for glue and stylus sprite integration

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp'),
     jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
     nib = require('nib'),
-    // sprites = require('gulp.spritesmith'),
     glue = require("gulp-sprite-glue"),
     watch = require('gulp-watch'),
     livereload = require('gulp-livereload'),
+    insert = require('gulp-insert'),
+    rename = require('gulp-rename'),
     config = require('./config.js').gulp,
     handler = require('./handler.js');
 
@@ -37,15 +38,25 @@ gulp.task('scripts', function () {
 });
 
 
-gulp.task('sprites', function () {
+gulp.task('sprites_img', function () {
   gulp.src('images/icons')
-      .pipe(glue({
-        css: 'build/static/styles/sprites/',
-        img: 'images/sprites/',
-        namespace: '',
-        spriteNamespace: '',
-        url: '../images/sprites/'
-      }));
+    .pipe(glue({
+      css: 'build/static/styles/sprites/',
+      img: 'images/sprites/',
+      namespace: '',
+      spriteNamespace: '',
+      url: '../images/sprites/'
+    }));
 });
 
+
+gulp.task('sprites_css_stylus', ['sprites_img'], function () {
+  gulp.src('build/static/styles/sprites/icons.css')
+    .pipe(insert.wrap('@css{', '}'))
+    .pipe(rename('sprites.styl'))
+    .pipe(gulp.dest('styles/modules/'));
+});
+
+
+gulp.task('sprites', ['sprites_img', 'sprites_css_stylus']);
 gulp.task('deploy', ['templates', 'styles', 'scripts']);
