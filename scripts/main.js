66437c2a5c4dc3c0fc175d8abe8aca91399c3602
zrobit <luismigre47@gmail.com>
2015-09-22 03:09:21 -0500

    add scripts base

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// IIFE - Immediately Invoked Function Expression
+(function($, window, document) {
+  // The $ is now locally scoped
+
+
+  // Listen for the jQuery ready event on the document
+  $(function() {
+    // The DOM is ready!
+
+
+    //End The DOM is ready
+  });
+  // The rest of the code goes here!
+
+
+}(window.jQuery, window, document));
+// The global jQuery object is passed as a parameter
