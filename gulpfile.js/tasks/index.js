b05076ac3edaa5f4bbba444164523ab2fd8d7f85
zrobit <luismigre47@gmail.com>
2016-01-08 11:38:37 -0500

    add icons support

--- gulpfile.js/tasks/index.js
+++ gulpfile.js/tasks/index.js
@@ -2,5 +2,5 @@ exports.watch = require("./watch.js");
 exports.templates = require("./templates.js");
 exports.styles = require("./styles.js");
 exports.scripts = require("./scripts.js");
-exports.icons = require("./icons.js");
+// exports.icons = require("./icons.js");
 exports.copy = require("./copy.js");
